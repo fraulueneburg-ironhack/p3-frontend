29b601de1a2e5fdf5ba5b550fa4b53793ee7971e
Michel Saber <michel-saber@hotmail.com>
2023-06-29 18:54:53 +0200

    comments deleted

--- src/components/BudgetForm.jsx
+++ src/components/BudgetForm.jsx
@@ -14,49 +14,14 @@ function BudgetForm() {
   const [savings, setSavings] = useState([
     { name: "My saving goal", amount: 0 },
   ]);
-  // console.log("THE CURRENCY", currency);
 
-  /* const [userObj, setUserObj] = useState({
-    currency: "€",
-    earnings: [{ name: "monthly earnings", amount: 0 }],
-    expenses: [
-      { name: "rent", amount: 0 },
-      { name: "electricity", amount: 0 },
-      { name: "heating", amount: 0 },
-    ],
-    savings: [{ name: "My saving goal", amount: 0 }],
-  }); */
   console.log(expenses);
-  //   const currency = userObj.currency;
 
   const handleChange = (index, e) => {
     const newExpenses = [...expenses];
     newExpenses[index].amount = Number(e.target.value);
     setExpenses(newExpenses);
   };
-  //you need the name of the input here
-  //this is the value of what you typed in the input
-  //spread the object to take everything that doesn't match the input name
-  //setUserObj({ ...userObj, [inputName]: inputValue });
-  // let inputName = e.target.name;
-  // let inputValue = e.target.value;
-
-  // console.log(inputName, inputValue);
-  /* if (inputName === "earnings") {
-      setEarnings({ ...earnings, [inputName]: inputValue });
-      console.log(earnings);
-    } else  */
-  // if (inputName === "expenses") {
-  //   setExpenses({ ...expenses, [inputName]: inputValue });
-  //   console.log(expenses);
-  // } else {
-  //   const parent = e.target.parentElement.parentElement.id;
-  //   //setUserObj({ ...userObj.expenses, userObj.expenses[inputName]: inputValue })
-  // }
-  //setUserObj({ ...userObj, [inputName]: inputValue });
-  /* console.log("NAME", inputName);
-    console.log("VALUE", inputValue); */
-  // console.log("THE USER OBJ", userObj);
 
   return (
     <>
@@ -131,48 +96,6 @@ function BudgetForm() {
             );
           })}
 
-          {/* <div className="grid">
-            <label>{expenses[0].name}</label>
-            <input
-              type="number"
-              min="0"
-              value={expenses[0].amount}
-              name={expenses[0].name}
-              onChange={handleChange}
-            />
-            <span className="currency">{`${currency}`}</span>
-            <button className="btn-delete-item" onSubmit={deleteItem}>
-              –
-            </button>
-          </div>
-          <div className="grid">
-            <label>{expenses[1].name}</label>
-            <input
-              type="number"
-              min="0"
-              value={expenses[1].amount}
-              name={expenses[1].name}
-              onChange={handleChange}
-            />
-            <span className="currency">{`${currency}`}</span>
-            <button className="btn-delete-item" onSubmit={deleteItem}>
-              –
-            </button>
-          </div>
-          <div className="grid">
-            <label>{expenses[2].name}</label>
-            <input
-              type="number"
-              min="0"
-              value={expenses[2].amount}
-              name={expenses[2].name}
-              onChange={handleChange}
-            />
-            <span className="currency">{`${currency}`}</span>
-            <button className="btn-delete-item" onSubmit={deleteItem}>
-              –
-            </button>
-          </div>*/}
           <div className="grid">
             <input
               type="text"
