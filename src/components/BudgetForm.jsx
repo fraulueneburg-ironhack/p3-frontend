19e65b79a28907dd653d688e031bea6088e63cf4
Wiebke Lueneburg <git@fraulueneburg.de>
2023-06-30 21:31:48 +0200

    added budget state, wrapped number groups in div.card, cleaned up

--- src/components/BudgetForm.jsx
+++ src/components/BudgetForm.jsx
@@ -1,102 +1,61 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function BudgetForm() {
+
+  // GENERAL FUNCTIONS 
+  const calculateTotal = (arr) => {
+    return arr.reduce((acc, curr) => { return acc + curr.amount }, 0)
+  }
+
+  // CURRENCY STATES
   const [currency, setCurrency] = useState("€");
-  //Earnings states
+
+  // EARNINGS STATES 
   const [earnings, setEarnings] = useState([
     { name: "monthly earnings", amount: 0 },
   ]);
+  const [earningsTotal, setEarningsTotal] = useState(calculateTotal(earnings));
   const [newEarningName, setNewEarningName] = useState("");
   const [newEarningAmount, setNewEarningAmount] = useState(0);
-  //Expenses states
+
+  // EXPENSES STATES
   const [expenses, setExpenses] = useState([
     { name: "rent", amount: 0 },
     { name: "electricity", amount: 0 },
     { name: "heating", amount: 0 },
   ]);
+  const [expensesTotal, setExpensesTotal] = useState(calculateTotal(expenses));
   const [newExpenseName, setNewExpenseName] = useState("");
   const [newExpenseAmount, setNewExpenseAmount] = useState(0);
 
-  //Savings states
+  // SAVINGS STATES
   const [savings, setSavings] = useState([
     { name: "My saving goal", amount: 0 },
   ]);
+  const [savingsTotal, setSavingsTotal] = useState(calculateTotal(savings));
   const [newSavingName, setNewSavingName] = useState("");
   const [newSavingAmount, setNewSavingAmount] = useState(0);
 
-  //Monthly Budget Overview state
-  const [budget, setBudget] = useState(0);
+  // MONTHLY BUDGET STATE
+  const [budget, setBudget] = useState(earningsTotal - expensesTotal - savingsTotal);
 
-  //function for calculating Budget
-  function calculateBudget(savings, expenses, earnings) {
-    const earningsTotal = earnings.reduce(function (acc, obj) {
-      return acc + obj.amount;
-    }, 0);
-    const expensesTotal = expenses.reduce(function (acc, obj) {
-      return acc + obj.amount;
-    }, 0);
-    const savingTotal = savings.reduce(function (acc, obj) {
-      return acc + obj.amount;
-    }, 0);
-
-    const result = earningsTotal - expensesTotal - savingTotal;
-    console.log(result, budget);
-    setBudget(result);
-    return result;
-  }
-  calculateBudget(savings, expenses, earnings);
-
-  //functions for expenses section
-  const handleExpenseAmountChange = (index, e) => {
-    const newExpenses = [...expenses];
-    newExpenses[index].amount = Number(e.target.value);
-    setExpenses(newExpenses);
-  };
-  const handleNewExpenseNameChange = (event) => {
-    setNewExpenseName(event.target.value);
-  };
-
-  const handleNewExpenseAmountChange = (event) => {
-    setNewExpenseAmount(Number(event.target.value));
-  };
-
-  const handleAddExpense = (event) => {
-    event.preventDefault();
-    const newExpense = {
-      name: newExpenseName,
-      amount: newExpenseAmount,
-    };
-    setExpenses([...expenses, newExpense]);
-    setNewExpenseName("");
-    setNewExpenseAmount(0);
-  };
-
-  const handleDeleteExpense = (index, e) => {
-    e.preventDefault();
-    let divToDelete = e.target.parentElement;
-    console.log(divToDelete);
-    console.log(index);
-    /* const filteredExpenses = expenses.filter((elem,index) => !index); */
-
-    const filteredExpenses = expenses.filter((elem, i) => {
-      if (i !== index) return elem;
-    });
-
-    console.log(filteredExpenses);
-    setExpenses(filteredExpenses);
-  };
-  //functions for Earnings section
+  // functions for Earnings section 
   const handleEarningAmountChange = (index, e) => {
     const newEarnings = [...earnings];
     newEarnings[index].amount = Number(e.target.value);
     setEarnings(newEarnings);
+    setEarningsTotal((calculateTotal(earnings)));
+    setBudget((calculateTotal(earnings)) - expensesTotal - savingsTotal);
   };
+
   const handleNewEarningNameChange = (event) => {
     setNewEarningName(event.target.value);
   };
 
   const handleNewEarningAmountChange = (event) => {
     setNewEarningAmount(Number(event.target.value));
+    setEarningsTotal((calculateTotal(earnings)));
+    setBudget((calculateTotal(earnings)) - expensesTotal - savingsTotal);
   };
 
   const handleAddEarning = (event) => {
@@ -106,29 +65,70 @@ function BudgetForm() {
       amount: newEarningAmount,
     };
     setEarnings([...earnings, newEarning]);
+    setEarningsTotal(calculateTotal([...earnings, newEarning]));
+    setBudget(calculateTotal([...earnings, newEarning]) - expensesTotal - savingsTotal);
     setNewEarningName("");
     setNewEarningAmount(0);
   };
+
   const handleDeleteEarning = (index, e) => {
     e.preventDefault();
-    let divToDelete = e.target.parentElement;
-    console.log(divToDelete);
-    console.log(index);
-    /* const filteredExpenses = expenses.filter((elem,index) => !index); */
-
     const filteredEarnings = earnings.filter((elem, i) => {
       if (i !== index) return elem;
     });
-
-    console.log(filteredEarnings);
     setEarnings(filteredEarnings);
+    setEarningsTotal((calculateTotal(filteredEarnings)));
+    setBudget((calculateTotal(filteredEarnings)) - expensesTotal - savingsTotal);
   };
 
-  //section for savings
+  // functions for expenses section 
+  const handleExpenseAmountChange = (index, e) => {
+    const newExpenses = [...expenses];
+    newExpenses[index].amount = Number(e.target.value);
+    setExpenses(newExpenses);
+    setExpensesTotal((calculateTotal(newExpenses)));
+    setBudget(earningsTotal - (calculateTotal(newExpenses)) - savingsTotal);
+  };
+  const handleNewExpenseNameChange = (event) => {
+    setNewExpenseName(event.target.value);
+  };
+
+  const handleNewExpenseAmountChange = (event) => {
+    setNewExpenseAmount(Number(event.target.value));
+    setExpensesTotal((calculateTotal(expenses)));
+    setBudget(earningsTotal - (calculateTotal(expenses)) - savingsTotal);
+  };
+
+  const handleAddExpense = (event) => {
+    event.preventDefault();
+    const newExpense = {
+      name: newExpenseName,
+      amount: newExpenseAmount,
+    };
+    setExpenses([...expenses, newExpense]);
+    setExpensesTotal((calculateTotal([...expenses, newExpense])));
+    setBudget(earningsTotal - ((calculateTotal([...expenses, newExpense]))) - savingsTotal);
+    setNewExpenseName("");
+    setNewExpenseAmount(0);
+  };
+
+  const handleDeleteExpense = (index, e) => {
+    e.preventDefault();
+    const filteredExpenses = expenses.filter((elem, i) => {
+      if (i !== index) return elem;
+    });
+    setExpenses(filteredExpenses);
+    setExpensesTotal((calculateTotal(filteredExpenses)));
+    setBudget(earningsTotal - (calculateTotal(filteredExpenses)) - savingsTotal);
+  };
+
+  // section for savings section 
   const handleSavingAmountChange = (index, e) => {
     const newSavings = [...savings];
     newSavings[index].amount = Number(e.target.value);
     setSavings(newSavings);
+    setSavingsTotal((calculateTotal(newSavings)));
+    setBudget(earningsTotal - expensesTotal - (calculateTotal(newSavings)));
   };
   const handleNewSavingNameChange = (event) => {
     setNewSavingName(event.target.value);
@@ -136,6 +136,8 @@ function BudgetForm() {
 
   const handleNewSavingAmountChange = (event) => {
     setNewSavingAmount(Number(event.target.value));
+    setSavingsTotal((calculateTotal(savings)));
+    setBudget(earningsTotal - expensesTotal - (calculateTotal(savings)));
   };
 
   const handleAddSaving = (event) => {
@@ -145,27 +147,26 @@ function BudgetForm() {
       amount: newSavingAmount,
     };
     setSavings([...savings, newSaving]);
+    setSavingsTotal((calculateTotal([...savings, newSaving])));
+    setBudget(earningsTotal - expensesTotal - (calculateTotal([...savings, newSaving])));
     setNewSavingName("");
     setNewSavingAmount(0);
   };
+
   const handleDeleteSaving = (index, e) => {
     e.preventDefault();
-    let divToDelete = e.target.parentElement;
-    console.log(divToDelete);
-    console.log(index);
-    /* const filteredExpenses = expenses.filter((elem,index) => !index); */
-
     const filteredSavings = savings.filter((elem, i) => {
       if (i !== index) return elem;
     });
-
-    console.log(filteredSavings);
     setSavings(filteredSavings);
+    setSavingsTotal((calculateTotal(filteredSavings)));
+    setBudget(earningsTotal - expensesTotal - (calculateTotal(filteredSavings)));
   };
 
   return (
     <>
       <form className="form-budget">
+
         <fieldset>
           <legend>Your currency</legend>
           <select name="currency" onChange={(e) => setCurrency(e.target.value)}>
@@ -176,27 +177,28 @@ function BudgetForm() {
         </fieldset>
         <fieldset id="earnings">
           <legend>Your earnings</legend>
-          {earnings.map((earning, index) => {
-            return (
-              <div key={index} className="grid">
-                <label>{earning.name}</label>
-                <input
-                  type="number"
-                  min="0"
-                  value={earning.amount}
-                  name={earning.name}
-                  onChange={(e) => handleEarningAmountChange(index, e)}
-                />
-                <span className="currency">{`${currency}`}</span>
-                <button
-                  className="btn-delete-item"
-                  onClick={(e) => handleDeleteEarning(index, e)}>
-                  –
-                </button>
-              </div>
-            );
-          })}
-
+          <div className="card">
+            {earnings.map((earning, index) => {
+              return (
+                <div key={index} className="grid">
+                  <label>{earning.name}</label>
+                  <input
+                    type="number"
+                    min="0"
+                    value={earning.amount}
+                    name={earning.name}
+                    onChange={(e) => handleEarningAmountChange(index, e)}
+                  />
+                  <span className="currency">{`${currency}`}</span>
+                  <button
+                    className="btn-delete-item"
+                    onClick={(e) => handleDeleteEarning(index, e)}>
+                    –
+                  </button>
+                </div>
+              );
+            })}
+          </div>
           <div className="grid">
             <input
               type="text"
@@ -216,30 +218,34 @@ function BudgetForm() {
             </button>
           </div>
         </fieldset>
+
         <fieldset id="expenses">
           <legend>Your expenses</legend>
-
-          {expenses.map((expense, index) => {
-            return (
-              <div key={index} className="grid">
-                <label>{expense.name}</label>
-                <input
-                  type="number"
-                  min="0"
-                  value={expense.amount}
-                  name={expense.name}
-                  onChange={(e) => handleExpenseAmountChange(index, e)}
-                />
-                <span className="currency">{`${currency}`}</span>
-                <button
-                  className="btn-delete-item"
-                  onClick={(e) => handleDeleteExpense(index, e)}>
-                  –
-                </button>
-              </div>
-            );
-          })}
-
+          <div className="card">
+            {expenses.map((expense, index) => {
+              return (
+                <div key={index} className="grid">
+                  <label>{expense.name}</label>
+                  <div className="input-group">
+                    <span className="input-group-text">-</span>
+                    <input
+                      type="number"
+                      min="0"
+                      value={expense.amount}
+                      name={expense.name}
+                      onChange={(e) => handleExpenseAmountChange(index, e)}
+                    />
+                  </div>
+                  <span className="currency">{`${currency}`}</span>
+                  <button
+                    className="btn-delete-item"
+                    onClick={(e) => handleDeleteExpense(index, e)}>
+                    –
+                  </button>
+                </div>
+              );
+            })}
+          </div>
           <div className="grid">
             <input
               type="text"
@@ -247,41 +253,49 @@ function BudgetForm() {
               onChange={handleNewExpenseNameChange}
               placeholder="name"
             />
-            <input
-              type="number"
-              min="0"
-              value={newExpenseAmount}
-              onChange={handleNewExpenseAmountChange}
-            />
+            <div className="input-group">
+              <span className="input-group-text">-</span>
+              <input
+                type="number"
+                min="0"
+                value={newExpenseAmount}
+                onChange={handleNewExpenseAmountChange}
+              />
+            </div>
             <span className="currency">{`${currency}`}</span>
             <button className="btn-add-item" onClick={handleAddExpense}>
               +
             </button>
           </div>
         </fieldset>
+
         <fieldset id="savings">
           <legend>Your saving goals</legend>
-          {savings.map((saving, index) => {
-            return (
-              <div key={index} className="grid">
-                <label>{saving.name}</label>
-                <input
-                  type="number"
-                  min="0"
-                  value={saving.amount}
-                  name={saving.name}
-                  onChange={(e) => handleSavingAmountChange(index, e)}
-                />
-                <span className="currency">{`${currency}`}</span>
-                <button
-                  className="btn-delete-item"
-                  onClick={(e) => handleDeleteSaving(index, e)}>
-                  –
-                </button>
-              </div>
-            );
-          })}
-
+          <div className="card">
+            {savings.map((saving, index) => {
+              return (
+                <div key={index} className="grid">
+                  <label>{saving.name}</label>
+                  <div className="input-group">
+                    <span className="input-group-text">-</span>
+                    <input
+                      type="number"
+                      min="0"
+                      value={saving.amount}
+                      name={saving.name}
+                      onChange={(e) => handleSavingAmountChange(index, e)}
+                    />
+                  </div>
+                  <span className="currency">{`${currency}`}</span>
+                  <button
+                    className="btn-delete-item"
+                    onClick={(e) => handleDeleteSaving(index, e)}>
+                    –
+                  </button>
+                </div>
+              );
+            })}
+          </div>
           <div className="grid">
             <input
               type="text"
@@ -289,12 +303,15 @@ function BudgetForm() {
               onChange={handleNewSavingNameChange}
               placeholder="name"
             />
-            <input
-              type="number"
-              min="0"
-              value={newSavingAmount}
-              onChange={handleNewSavingAmountChange}
-            />
+            <div className="input-group">
+              <span className="input-group-text">-</span>
+              <input
+                type="number"
+                min="0"
+                value={newSavingAmount}
+                onChange={handleNewSavingAmountChange}
+              />
+            </div>
             <span className="currency">{`${currency}`}</span>
             <button className="btn-add-item" onClick={handleAddSaving}>
               +
